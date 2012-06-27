23ec170164ad292859b49a584e02693266d7fc72
Nikita <nike-17@ya.ru>
2012-06-27 13:53:23 +0700

    add server

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ go-tcp-server
 =============
 
 Simple tcp server for tcp servers contest
+=============
+Running
+
+go run tcp-server.go
---
+++ tcp-server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net"
+	"os"
+	"fmt"
+)
+
+func main() {
+
+	service := ":6666"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		handleClient(conn)
+		conn.Close()
+	}
+}
+
+func handleClient(conn net.Conn) {
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
